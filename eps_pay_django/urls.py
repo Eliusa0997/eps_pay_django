051adcc45e3b69104ad2dce94cc0de79f978cc59
eliusa0997 <eliusaomer@gmail.com>
2026-02-20 22:51:45 +0200

    .gitignor

--- eps_pay_django/urls.py
+++ eps_pay_django/urls.py
@@ -24,8 +24,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/register/', register),
     path('admin/', admin.site.urls),
+    path('api/register/', register),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
