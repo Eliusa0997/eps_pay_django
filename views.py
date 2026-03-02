2dd1b8be768e2c6ec3347036f43a250e1c0504f3
eliusa0997 <eliusaomer@gmail.com>
2026-03-02 22:23:32 +0200

    tring

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from accounts.models import Profile 
 
 @api_view(['POST'])
 def register(request):
