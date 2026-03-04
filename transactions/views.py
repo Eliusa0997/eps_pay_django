c41690ce9e4b81e510fe03471ada172a9e816b9f
eliusa0997 <eliusaomer@gmail.com>
2026-03-04 15:56:44 +0200

    transactons histry

--- transactions/views.py
+++ transactions/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .serializers import TransferSerializer, DepositSerializer, WithdrawSerializer, ElectricitySerializer, WaterSerializer, InternetSerializer, MobileRechargeSerializer      
+from .serializers import TransferSerializer, DepositSerializer, WithdrawSerializer, ElectricitySerializer, WaterSerializer, InternetSerializer, MobileRechargeSerializer, TransactionSerializer 
 from .models import Transaction
 # transfer payment  
 class TransferView(APIView):
