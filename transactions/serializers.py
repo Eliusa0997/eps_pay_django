97549808b2055035a32ff4bd8980b0b21bcb907f
eliusa0997 <eliusaomer@gmail.com>
2026-03-06 19:49:01 +0200

    modifi

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -287,9 +287,13 @@ class TransactionSerializer(serializers.ModelSerializer):
             "transaction_type",
             "amount",
             "timestamp",
+            "sender_full_name",
             "receiver_full_name"
         ]
-
+    def get_sender_full_name(self, obj):
+        if obj.sender and obj.sender.user:
+            return f"{obj.sender.user.first_name} {obj.sender.user.last_name}"
+        return None
     def get_receiver_full_name(self, obj):
         if obj.receiver and obj.receiver.user:
             return f"{obj.receiver.user.first_name} {obj.receiver.user.last_name}"
