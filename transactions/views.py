690d0fc91096655af97e73f76cafc15aa1cce17a
eliusa0997 <eliusaomer@gmail.com>
2026-03-08 15:34:26 +0200

    bills refactoring

--- transactions/views.py
+++ transactions/views.py
@@ -158,15 +158,6 @@ class TransactionHistoryView(APIView):
         return Response(serializer.data)    
 
 
-
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Transaction
-from .serializers import BillTransactionSerializer
-
-
 # bills transactions history    
 class BillsTransactionsHistoryView(APIView):
     permission_classes = [IsAuthenticated]
