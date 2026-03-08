04ec4d4ea3b0bf605617c156efe727d23364e8bc
eliusa0997 <eliusaomer@gmail.com>
2026-03-08 15:38:33 +0200

    bills refactoring

--- transactions/views.py
+++ transactions/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .serializers import TransferSerializer, DepositSerializer, WithdrawSerializer, ElectricitySerializer, WaterSerializer, InternetSerializer, MobileRechargeSerializer, TransactionSerializer, BillTransactionSerializer
+from .serializers import TransferSerializer, DepositSerializer, WithdrawSerializer, ElectricitySerializer, WaterSerializer, InternetSerializer, MobileRechargeSerializer, TransactionSerializer, BillTransactionHistorySerializer
 from .models import Transaction
 
 # transfer payment  
